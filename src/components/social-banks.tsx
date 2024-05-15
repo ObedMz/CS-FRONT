16483aaad8ff899e2e3b8ae75037cb182cb89db7
Obed <obedmontano43@gmail.com>
2024-05-15 14:51:56 -0500

    Added .env values to social link and bank accounts

--- src/components/social-banks.tsx
+++ src/components/social-banks.tsx
@@ -2,7 +2,7 @@ import { Landmark } from "lucide-react";
 import Image from "next/image";
 
 export function TetherOption(){
-    const url = 'https://www.thether.io/';
+    const url = process.env.TETHER_URL;
     return (
         <a href={url} className="w-[100px] h-[25px] flex">
             <Image src="/tether-logo.png" alt="thether" width={80} height={80} loading='eager' sizes="contain" priority={true} />
@@ -10,7 +10,7 @@ export function TetherOption(){
     )
 }
 export function BinanceOption(){
-    const url = 'https://www.binance.com/';
+    const url = process.env.BINANCE_URL;
     return (
         <a href={url} className="w-[100px] h-[30px] flex items-center">
             <Image src="/binance-logo.svg" alt="thether" width={100} height={100} loading='lazy' sizes="contain" />
@@ -20,7 +20,7 @@ export function BinanceOption(){
 
 
 export function BankOption(){
-    const url = 'https://www.bankAcount.com/';
+    const url = process.env.DEFAULT_BANK_URL;
     return (
         <a href={url} className="text-sm w-[150px] h-[30px] flex items-center gap-1 rounded-full border border-white py-4 px-3">
                 <Landmark />
--- src/layouts/social-bar/index.tsx
+++ src/layouts/social-bar/index.tsx
@@ -3,9 +3,9 @@ import { Facebook, Instagram, Twitter } from "lucide-react";
 export default function NavSocialBar() {
     return (
         <div className="fixed z-30 bg-[#1B1D24] top-[40%] text-white flex flex-col p-3 shadow-xl gap-5 right-rounded hover:scale-105 transition-all cursor-pointer">
-            <a href="https://www.facebook.com" className=""><Facebook /></a>
-            <a href="https://www.twitter.com" className=""><Twitter /></a>
-            <a href="https://www.instagram.com" className=""><Instagram /></a>
+            <a href={process.env.FACEBOOK_URL} className=""><Facebook /></a>
+            <a href={process.env.TWITTER_URL} className=""><Twitter /></a>
+            <a href={process.env.INSTAGRAM_URL} className=""><Instagram /></a>
         </div>
     )
 };
