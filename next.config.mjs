83401be31e819c3e417e7996a2b9d13895499395
Obed <obedmontano43@gmail.com>
2024-05-20 17:56:44 -0500

    fix http error with DNS

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          }]},
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        }]},
+  experimental: {
+          serverActions: {
+              allowedOrigins: ['jabbustore.com'],
+          },
+        },
 };
 
 export default nextConfig;
